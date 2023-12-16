f5b58e07f93581ca4655f768ba89b4b328ed8fb1
BrC_Habeda <borischabeda@gmail.com>
2023-12-16 23:15:15 +0300

    🐎 ci(update): CI

--- scripts/series_generate.sql
+++ scripts/series_generate.sql
@@ -1,4 +1,13 @@
+CREATE TABLE sales (
+  sale_date DATE,
+  amount DECIMAL(10, 2)
+);
 
+-- Insert some sample data
+INSERT INTO sales (sale_date, amount) VALUES
+  ('2023-01-01', 100.50),
+  ('2023-01-02', 150.75),
+  ('2023-01-04', 200.00);
 -- Generate a series of dates for a specific range
 WITH DateSeries AS (
   SELECT generate_series(
