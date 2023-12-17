f02d5f08155ead910e3f99b834714b020c3af3e3
BrC_Habeda <borischabeda@gmail.com>
2023-12-17 21:59:27 +0300

    🐳 chore(update): sql test

--- scripts/series_generate.sql
+++ scripts/series_generate.sql
@@ -6,6 +6,7 @@ CREATE TABLE sales (
 -- Insert some sample data
 INSERT INTO sales (sale_date, amount) VALUES
   ('2023-01-01', 100.50),
+  ('2023-01-01', 50.50),
   ('2023-01-02', 150.75),
   ('2023-01-04', 200.00);
   
