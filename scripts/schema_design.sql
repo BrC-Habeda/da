65ebb6be833ee3473328305a320823ffe86f0255
BrC_Habeda <borischabeda@gmail.com>
2023-12-17 22:13:20 +0300

    update schema_design.sql

--- scripts/schema_design.sql
+++ scripts/schema_design.sql
@@ -1,31 +1,31 @@
 -- Schema DesignAndModeling
-CREATE SCHEMA IF NOT EXISTS "DesignAndModeling";
+CREATE SCHEMA IF NOT EXISTS DesignAndModeling;
 
 -- Table DesignAndModeling.Course
-CREATE TABLE IF NOT EXISTS "DesignAndModeling"."Course" (
-  "CourseID" VARCHAR(10) PRIMARY KEY,
-  "CourseName" VARCHAR(100)
+CREATE TABLE IF NOT EXISTS DesignAndModeling.Course (
+  CourseID VARCHAR(10) PRIMARY KEY,
+  CourseName VARCHAR(100)
 );
 
 -- Table DesignAndModeling.Student
-CREATE TABLE IF NOT EXISTS "DesignAndModeling"."Student" (
-  "StudentID" SERIAL PRIMARY KEY,
-  "StudentName" VARCHAR(100),
-  "DateOfBirth" DATE,
-  "Major" VARCHAR(100),
-  "ContactNumber" VARCHAR(15)
+CREATE TABLE IF NOT EXISTS DesignAndModeling.Student (
+  StudentID SERIAL PRIMARY KEY,
+  StudentName VARCHAR(100),
+  DateOfBirth DATE,
+  Major VARCHAR(100),
+  ContactNumber VARCHAR(20)
 );
 
 -- Table DesignAndModeling.StudentCourses
-CREATE TABLE IF NOT EXISTS "DesignAndModeling"."StudentCourses" (
-  "EnrollmentID" SERIAL PRIMARY KEY,
-  "StudentID" BIGINT REFERENCES "DesignAndModeling"."Student" ("StudentID"),
-  "CourseID" VARCHAR(10) UNIQUE REFERENCES "DesignAndModeling"."Course" ("CourseID")
+CREATE TABLE IF NOT EXISTS DesignAndModeling.StudentCourses (
+  EnrollmentID SERIAL PRIMARY KEY,
+  StudentID BIGINT REFERENCES DesignAndModeling.Student (StudentID),
+  CourseID VARCHAR(10) UNIQUE REFERENCES DesignAndModeling.Course (CourseID)
 );
 
  -- Inserting data into the Student table 
 
-INSERT INTO "DesignAndModeling"."Student" (StudentName, DateOfBirth, Major, ContactNumber) 
+INSERT INTO DesignAndModeling.Student (StudentName, DateOfBirth, Major, ContactNumber) 
 VALUES  
 ('John Doe', '2000-05-15', 'Computer Science', '+254705467890'), 
 ('Jane Smith', '2001-03-12', 'Physics', '+254745678913'), 
@@ -33,7 +33,7 @@ VALUES
 
 -- Inserting data into the Course table 
 
-INSERT INTO "DesignAndModeling"."Course" (CourseID, CourseName) 
+INSERT INTO DesignAndModeling.Course (CourseID, CourseName) 
 VALUES  
 ('CS101', 'Introduction to Programming'), 
 ('PH102', 'Advanced Physics'), 
@@ -41,7 +41,7 @@ VALUES
 ('DSC101', 'Introduction to Data Science'); 
 
 -- Insert data into the StudentCourses table
-INSERT INTO "DesignAndModeling"."StudentCourses" (StudentID, CourseID)
+INSERT INTO DesignAndModeling.StudentCourses (StudentID, CourseID)
 VALUES
     (1, 'CS101'),
     (2, 'PH102'),
@@ -52,4 +52,4 @@ VALUES
 -- SELECT * data in the Student.Courses table
 
 SELECT StudentID
-FROM "DesignAndModeling"."StudentCourses";
+FROM DesignAndModeling.StudentCourses;
