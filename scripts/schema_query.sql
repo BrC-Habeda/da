7bced3dc234af6d93742b5e87c6555cac4748a7e
BrC_Habeda <borischabeda@gmail.com>
2023-12-24 19:18:58 +0300

    🦄 refactor: schema test

---
+++ scripts/schema_query.sql
@@ -0,0 +1,55 @@
+-- Schema DesignAndModeling
+CREATE SCHEMA IF NOT EXISTS DesignAndModeling;
+
+-- Table DesignAndModeling.Course
+CREATE TABLE IF NOT EXISTS DesignAndModeling.Course (
+  CourseID VARCHAR(10) PRIMARY KEY,
+  CourseName VARCHAR(100)
+);
+
+-- Domain to only accept Kenyan Numbers
+CREATE DOMAIN phone_number AS VARCHAR(15)
+  NOT NULL
+  CHECK (VALUE ~ '^\+254\d{9}$');
+
+-- Table DesignAndModeling.Student
+CREATE TABLE IF NOT EXISTS DesignAndModeling.Student (
+  StudentID SERIAL PRIMARY KEY,
+  StudentName VARCHAR(100),
+  DateOfBirth DATE,
+  Major VARCHAR(100),
+  Phone phone_number
+);
+
+-- Table DesignAndModeling.StudentCourses
+CREATE TABLE IF NOT EXISTS DesignAndModeling.StudentCourses (
+  EnrollmentID SERIAL PRIMARY KEY,
+  StudentID BIGINT REFERENCES DesignAndModeling.Student (StudentID),
+  CourseID VARCHAR(10) REFERENCES DesignAndModeling.Course (CourseID)
+);
+
+ -- Inserting data into the Student table 
+
+INSERT INTO DesignAndModeling.Student (StudentName, DateOfBirth, Major, Phone) 
+VALUES  
+('John Doe', '2000-05-15', 'Computer Science', '+254705467890'), 
+('Jane Smith', '2001-03-12', 'Physics', '+254745678913'), 
+('Jim Bean', '1999-07-25', 'Mathematics', '+254700678925'); 
+
+-- Inserting data into the Course table 
+
+INSERT INTO DesignAndModeling.Course (CourseID, CourseName) 
+VALUES  
+('CS101', 'Introduction to Programming'), 
+('PH102', 'Advanced Physics'), 
+('MA103', 'Calculus II'), 
+('DSC101', 'Introduction to Data Science'); 
+
+-- Insert data into the StudentCourses table
+INSERT INTO DesignAndModeling.StudentCourses (StudentID, CourseID)
+VALUES
+    (1, 'CS101'),
+    (2, 'PH102'),
+    (3, 'MA103'),
+    (1, 'DSC101'),
+    (2, 'MA103');
--- tests/test_schema_design.py
+++ tests/test_schema_design.py
@@ -1,16 +1,17 @@
 import os
 from sqlalchemy import create_engine, text
 
+
 def test_query():
     # Get the path to the SQL file in the root folder
-    sql_file_path = os.path.join(os.path.dirname(__file__), '../scripts/db_design.sql')
+    sql_file_path = os.path.join(os.path.dirname(__file__), "../scripts/db_design.sql")
 
     # Read the SQL query from the file
-    with open(sql_file_path, 'r') as sql_file:
+    with open(sql_file_path, "r") as sql_file:
         sql_query = sql_file.read()
 
     # Connect to the database and execute the SQL query
-    engine = create_engine('postgresql://postgres:postgres@localhost:5432/testdb')
+    engine = create_engine("postgresql://postgres:postgres@localhost:5432/testdb")
     conn = engine.connect()
 
     try:
@@ -20,7 +21,9 @@ def test_query():
 
         # Check the actual result count against the expected count
         expected_count = 5
-        assert len(rows) == expected_count, f"Expected {expected_count} rows, but got {len(rows)}"
+        assert (
+            len(rows) == expected_count
+        ), f"Expected {expected_count} rows, but got {len(rows)}"
 
         # You can add additional checks here if needed
 
@@ -30,27 +33,37 @@ def test_query():
         # Close the database connection
         conn.close()
 
+
 def test_schema():
     # Get the path to the SQL file in the root folder
-    sql_file_path = os.path.join(os.path.dirname(__file__), '../scripts/schema_test.sql')
+    sql_file_path = os.path.join(
+        os.path.dirname(__file__), "../scripts/schema_query.sql"
+    )
 
+    sqltest_file_path = os.path.join(
+        os.path.dirname(__file__), "../scripts/schema_test.sql"
+    )
     # Read the SQL query from the file
-    with open(sql_file_path, 'r') as sql_file:
-        sql_query = sql_file.read()      
-    # Get the Schema Name 
-    schema_name = 'DesignAndModeling'
+    with open(sql_file_path, "r") as sql_file:
+        sql_query = sql_file.read()
+
+    with open(sqltest_file_path, "r") as sqltest_file:
+        sql_test = sqltest_file.read()
 
     # Connect to the database and execute the SQL query
-    engine = create_engine('postgresql://postgres:postgres@localhost:5432/testdb')
+    engine = create_engine("postgresql://postgres:postgres@localhost:5432/testdb")
     conn = engine.connect()
 
     try:
-        result = conn.execute(text(sql_query))
+        # Seed the data into the database
+        conn.execute(text(sql_query))
+
+        result = conn.execute(text(sql_test))
         # Fetch all rows from the result set
         rows = result.fetchall()
 
         # Check if there are any tables in the schema
-        assert len(rows) > 0, f"No tables found in the schema '{schema_name}'"
+        assert len(rows) > 0, f"No tables found in the schema"
 
         print("Schema test passed successfully!")
 
@@ -58,6 +71,7 @@ def test_schema():
         # Close the database connection
         conn.close()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     test_query()
     test_schema()
