6cf62a045c322c203eaed688c38d0c93b65d4063
BrC_Habeda <borischabeda@gmail.com>
2023-12-29 18:10:27 +0300

    🎈 perf: update models.py

--- models.py
+++ models.py
@@ -1,7 +1,20 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import declarative_base, relationship
 
+import re
+
 Base = declarative_base()
+
+
+class KenyanPhoneNumber:
+    def __init__(self, number):
+        if not self.is_valid(number):
+            raise ValueError("Invalid Phone Number")
+        self.number = number
+
+    def is_valid(self, number):
+        pattern = re.compile(r"^\+2547[0-9]{8}$")
+        return bool(pattern.match(number))
 
 
 class User(Base):
@@ -19,5 +32,10 @@ class Address(Base):
     id = Column(Integer, primary_key=True)
     email = Column(String)
     user_id = Column(Integer, ForeignKey("users.id"))
+    _phone_number = Column("phone_number", String, nullable=False)
+
+    @validates("_phone_number")
+    def validate_phone_number(self, key, phone_number):
+        return KenyanPhoneNumber(phone_number).number
 
     user = relationship("User", back_populates="addresses")
