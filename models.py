635a80fe9e916fb3693dc4b6b40b9b916276063a
BrC_Habeda <borischabeda@gmail.com>
2024-01-02 07:59:44 +0300

    ✨ feat: added unique constraint

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 import re
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, func, UniqueConstraint
 from sqlalchemy.orm import validates
 from sqlalchemy.orm import declarative_base, relationship
 
@@ -40,3 +40,29 @@ class Address(Base):
         return KenyanPhoneNumber(phone_number).number
 
     user = relationship("User", back_populates="addresses")
+    
+class Room(Base):
+    __tablename__ = "rooms"
+    
+    id = Column(Integer, primary_key=True)
+    room_number = Column(String, unique=True, nullable=False)
+    
+    #Define a relationship with the reservation table
+    reservations = relationship('Reservation', back_populates='room')
+    
+class Reservation(Base):
+    __tablename__ = 'reservations'
+    
+    id = Column(Integer, primary_key=True)
+    guest_name = Column(String, nullable=False)
+    check_in = Column(DateTime(timezone=True), default=func.now())
+    check_out = Column(DateTime(timezone=True), nullable=False)
+    
+    # Foreign Key relationship to rooms
+    room_id = Column(Integer, ForeignKey('rooms.id'))
+    room = relationship('Room', back_populates='reservations')
+    
+    # Define an exclusion constraint to prevent overlapping reservations
+    __table_args__ = (
+        UniqueConstraint('room_id', 'check_in', 'check_out', name='unique_reservation'),
+    )
