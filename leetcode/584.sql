235fd4b56fe2289977edee63b9d805f653650954
BrC_Habeda <borischabeda@gmail.com>
2024-01-18 23:17:11 +0300

    🔧 build: 584

--- leetcode/584.sql
+++ leetcode/584.sql
@@ -6,3 +6,14 @@ CREATE TABLE customer(
     name varchar(255),
     referee_id int
 );
+-- Insert data into customers table
+
+INSERT INTO customer(id,name,referee_id)
+VALUES
+(1,'Will',null),
+(1,'Jane',null),
+(1,'Alex',2),
+(1,'Bill',null),
+(1,'Zack',1),
+(1,'Mark',2);
+
