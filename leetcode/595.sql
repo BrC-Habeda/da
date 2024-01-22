730087cec3eb1a0a428e4aab618502aeb764b010
BrC_Habeda <borischabeda@gmail.com>
2024-01-22 08:42:31 +0300

    ✨ feat: create table world

--- leetcode/595.sql
+++ leetcode/595.sql
@@ -1,1 +1,10 @@
 -- 595. Big Countries
+
+-- Create table World
+CREATE TABLE world(
+    name varchar(255),
+    continent varchar(255),
+    area int(11),
+    population int(11),
+    gdp bigint
+)
