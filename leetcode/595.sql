03463d9a0f8191755d9f8df867d9e7f65b9c0058
BrC_Habeda <borischabeda@gmail.com>
2024-01-22 08:59:29 +0300

    🧪 test: big countries

--- leetcode/595.sql
+++ leetcode/595.sql
@@ -17,3 +17,12 @@ VALUES
 ('Algeria','Africa',2381741,37100000,188681000000),
 ('Andorra','Europe',468,78115,3712000000),
 ('Angola','Africa',1246700,20609294,100990000000):
+
+-- A country is big if:
+--it has an area of at least three million (i.e., 3000000 km2), or
+--it has a population of at least twenty-five million (i.e., 25000000).
+--Write a solution to find the name, population, and area of the big countries
+
+SELECT name,population,area
+FROM world
+WHERE area >=3000000 OR population >= 25000000;
