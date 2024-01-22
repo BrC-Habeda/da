d536a6eecaa1b9ddd3a7967563862267683fe88c
BrC_Habeda <borischabeda@gmail.com>
2024-01-22 09:14:33 +0300

    🐞 fix:SyntaxError

--- leetcode/595.sql
+++ leetcode/595.sql
@@ -17,7 +17,7 @@ VALUES
 ('Albania','Europe',28748,2831741,12960000000),
 ('Algeria','Africa',2381741,37100000,188681000000),
 ('Andorra','Europe',468,78115,3712000000),
-('Angola','Africa',1246700,20609294,100990000000):
+('Angola','Africa',1246700,20609294,100990000000);
 
 -- A country is big if:
 --it has an area of at least three million (i.e., 3000000 km2), or
