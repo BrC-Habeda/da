67d0bc7a4c3e8345eb9940dba7b308bdf9123041
BrC_Habeda <borischabeda@gmail.com>
2024-01-23 12:58:12 +0300

    🐞 fix:type cast

--- leetcode/1148.sql
+++ leetcode/1148.sql
@@ -12,13 +12,13 @@ CREATE TABLE views(
 
 INSERT INTO views(article_id,author_id,viewer_id,view_date)
 VALUES
-(1,3,5,2019-08-01),
-(1,3,6,2019-08-02),
-(2,7,7,2019-08-01),
-(2,7,6,2019-08-02),
-(4,7,1,2019-07-22),
-(3,4,4,2019-07-21),
-(3,4,4,2019-07-21);
+(1,3,5,'2019-08-01'),
+(1,3,6,'2019-08-02'),
+(2,7,7,'2019-08-01'),
+(2,7,6,'2019-08-02'),
+(4,7,1,'2019-07-22'),
+(3,4,4,'2019-07-21'),
+(3,4,4,'2019-07-21');
 
 --Write a solution to find all the authors that viewed at least one of their own articles
 --Return the result table sorted by id in ascending order
