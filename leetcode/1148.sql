a0de60c3f273ba4624fd480c9bc9f1a12e959e61
BrC_Habeda <borischabeda@gmail.com>
2024-01-23 12:24:05 +0300

    ✨ feat:data insert

--- leetcode/1148.sql
+++ leetcode/1148.sql
@@ -7,3 +7,15 @@ CREATE TABLE views(
     viewer_id int,
     view_date date
 );
+
+-- Insert data into the table
+
+INSERT INTO views(article_id,author_id,viewer_id,view_date)
+VALUES
+(1,3,5,2019-08-01),
+(1,3,6,2019-08-02),
+(2,7,7,2019-08-01),
+(2,7,6,2019-08-02),
+(4,7,1,2019-07-22),
+(3,4,4,2019-07-21),
+(3,4,4,2019-07-21);
