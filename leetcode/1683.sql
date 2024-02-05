4bb486776d48335990ef1b0cb62f47eb9604a1a5
BrC_Habeda <borischabeda@gmail.com>
2024-02-05 19:59:25 +0300

    🔧 build: table

--- leetcode/1683.sql
+++ leetcode/1683.sql
@@ -1,1 +1,9 @@
 -- 1683. Invalid Tweets
+-- Write a solution to find the IDs of the invalid tweets
+-- A tweet invalid if the number of characters used in the content of the tweet is > 15
+
+-- CREATE THE TABLE 
+CREATE TABLE tweets(
+    tweet_id INT,
+    content text
+);
