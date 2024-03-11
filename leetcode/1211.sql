fedc7e6d36071eb09fa535b720a4c392b746d4a7
BrC_Habeda <borischabeda@gmail.com>
2024-03-11 15:50:30 +0300

    🧪 test:1211

--- leetcode/1211.sql
+++ leetcode/1211.sql
@@ -13,3 +13,24 @@ INSERT INTO Queries (query_name, result, position, rating) VALUES
 ('Cat', 'Siamese', 3, 3),
 ('Cat', 'Sphynx', 7, 4);
 
+/* --We define query quality as:
+
+    The average of the ratio between query rating and its position.
+
+We also define poor query percentage as:
+
+    The percentage of all queries with rating less than 3.
+
+Write a solution to find each query_name, the quality and poor_query_percentage.
+
+Both quality and poor_query_percentage should be rounded to 2 decimal places.
+
+Return the result table in any order. */
+
+
+SELECT query_name,
+    ROUND(AVG(CAST(rating AS DECIMAL) / position), 2) AS quality,
+    ROUND((SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END) * 100.0) / COUNT(*), 2) AS poor_query_percentage
+FROM Queries
+WHERE query_name IS NOT NULL
+GROUP BY query_name;
