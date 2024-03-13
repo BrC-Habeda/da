3c8118bd7531cf62c6643045ea552106b154f735
BrC_Habeda <borischabeda@gmail.com>
2024-03-13 16:38:47 +0300

    🧪 test:1193

--- leetcode/1193.sql
+++ leetcode/1193.sql
@@ -10,3 +10,25 @@ CREATE TABLE transactions(
     amount int,
     trans_date date
 );
+
+INSERT INTO transactions(id,country,state,amount,trans_date)
+VALUES
+(121,'US','approved',1000,'2018-12-18'),
+(122,'US','declined',2000,'2018-12-19'),
+(123,'US','approved',2000,'2019-01-01'),
+(124,'DE','approved',2000,'2019-01-07');
+
+-- Write a query to find each month and country, the number of transactions and their
+-- total amount, the number of approved transactions and their total amount
+
+SELECT 
+    TO_CHAR(DATE_TRUNC('MONTH', trans_date), 'YYYY-MM') AS month,
+    country,
+    COUNT(*) AS trans_count,
+    SUM(amount) AS trans_total_amount,
+    COUNT(*) FILTER (WHERE state = 'approved') AS approved_count,
+    SUM(amount) FILTER (WHERE state = 'approved') AS approved_total_amount
+FROM 
+    transactions
+GROUP BY 
+    month, country;
