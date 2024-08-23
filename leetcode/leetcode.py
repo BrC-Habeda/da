89d5a1617d542e440117e14c0dbc19cee3d940d6
BrC_Habeda <borischabeda@gmail.com>
2024-08-23 12:55:40 +0300

    📃 docs:

--- leetcode/leetcode.py
+++ leetcode/leetcode.py
@@ -1,20 +1,16 @@
 import os
 from sqlalchemy import create_engine, text
 
-
 def run_tests(sql_files):
+    engine = create_engine("postgresql://postgres:postgres@localhost:5432/testdb")
+    
     for sql_file in sql_files:
-        # Read the sql query from the file
-        with open(sql_file, "r") as file:
-            sql_query = file.read()
-
-        # Connect to the database and execute the query
-        engine = create_engine("postgresql://postgres:postgres@localhost:5432/testdb")
-        conn = engine.connect()
-        
-
-        # Run the tests for the query
         try:
+            # Read the SQL query from the file
+            with open(sql_file, "r") as file:
+                sql_query = file.read()
+
+            # Connect to the database and execute the query
             with engine.connect() as conn:
                 # Execute the query
                 result = conn.execute(text(sql_query))
@@ -22,162 +18,143 @@ def run_tests(sql_files):
                 # Fetch all rows from the query
                 rows = result.fetchall()
                 
-                
-
                 # Perform assertions based on the file name or content
                 if "1757" in sql_file:
                     expected_count = 2
-                    assert (
-                        len(rows) == expected_count
-                    ), f"Test failed for {sql_file}: Expected {expected_count} but found {len(rows)}"
+                    assert len(rows) == expected_count, (
+                        f"Test failed for {sql_file}: Expected {expected_count} but found {len(rows)}"
+                    )
 
-                # Add more assertions for other files as needed
                 if "584" in sql_file:
                     expected_count = 4
-                    assert (
-                        len(rows) == expected_count
-                    ), f"Test failed for {sql_file}: Expected {expected_count} but found {len(rows)}"
+                    assert len(rows) == expected_count, (
+                        f"Test failed for {sql_file}: Expected {expected_count} but found {len(rows)}"
+                    )
 
-                # Big Countries
                 if "595" in sql_file:
                     expected_count = 2
-                    assert (
-                        len(rows) == expected_count
-                    ), f"Test failed for {sql_file}: Expected {expected_count} but found {len(rows)}"
+                    assert len(rows) == expected_count, (
+                        f"Test failed for {sql_file}: Expected {expected_count} but found {len(rows)}"
+                    )
 
-                # Invalid Tweets
                 if "1683" in sql_file:
                     expected_count = 1
-                    assert (
-                        len(rows) == expected_count
-                    ), f"Test failed for {sql_file}: Expected {expected_count} but fount {len(rows)}"
+                    assert len(rows) == expected_count, (
+                        f"Test failed for {sql_file}: Expected {expected_count} but found {len(rows)}"
+                    )
 
-                # Article Views
                 if "1148" in sql_file:
                     expected_count = 2
-                    assert (
-                        len(rows) == expected_count
-                    ), f"Test failed for {sql_file}: Expected {expected_count} but found {len(rows)}"
+                    assert len(rows) == expected_count, (
+                        f"Test failed for {sql_file}: Expected {expected_count} but found {len(rows)}"
+                    )
 
-                # Replace EmployeeID
                 if "1378" in sql_file:
                     expected_count = 5
-                    assert (
-                        len(rows) == expected_count
-                    ), f"Test failed for {sql_file}: Expected {expected_count} but found {len(rows)}"
+                    assert len(rows) == expected_count, (
+                        f"Test failed for {sql_file}: Expected {expected_count} but found {len(rows)}"
+                    )
 
-                # Product Sales Analysis I
                 if "1068" in sql_file:
                     expected_count = 3
-                    assert (
-                        len(rows) == expected_count
-                    ), f"Test failed for {sql_file}: Expected {expected_count} but found {len(rows)}"
+                    assert len(rows) == expected_count, (
+                        f"Test failed for {sql_file}: Expected {expected_count} but found {len(rows)}"
+                    )
 
-                # Customers who visited without making any transactions
                 if "1581" in sql_file:
                     expected_count = 3
-                    assert (
-                        len(rows) == expected_count
-                    ), f"Test failed for {sql_file}: Expected {expected_count} but found {len(rows)}"
+                    assert len(rows) == expected_count, (
+                        f"Test failed for {sql_file}: Expected {expected_count} but found {len(rows)}"
+                    )
 
-                # Rising temperature
                 if "197" in sql_file:
                     expected_count = 2
-                    assert (
-                        len(rows) == expected_count
-                    ), f"Test failed for {sql_file}: Expected {expected_count} but found {len(rows)}"
+                    assert len(rows) == expected_count, (
+                        f"Test failed for {sql_file}: Expected {expected_count} but found {len(rows)}"
+                    )
 
-                # Average time of process per machine
                 if "1661" in sql_file:
                     expected_count = 3
-                    assert (
-                        len(rows) == expected_count
-                    ), f"Test failed for {sql_file}: Expectation differences"
+                    assert len(rows) == expected_count, (
+                        f"Test failed for {sql_file}: Expected {expected_count} but found {len(rows)}"
+                    )
 
-                # Employee Bonus
                 if "577" in sql_file:
                     expected_count = 3
-                    assert (
-                        len(rows) == expected_count
-                    ), f"Test failed for {sql_file}: Expectation differences"
+                    assert len(rows) == expected_count, (
+                        f"Test failed for {sql_file}: Expected {expected_count} but found {len(rows)}"
+                    )
 
-                # Students and Examinations
                 if "1280" in sql_file:
                     expected_count = 12
-                    assert (
-                        len(rows) == expected_count
-                    ), f"Test failed for {sql_file}: Expectation differences"
+                    assert len(rows) == expected_count, (
+                        f"Test failed for {sql_file}: Expected {expected_count} but found {len(rows)}"
+                    )
 
-                # Managers with direct reports
                 if "570" in sql_file:
                     expected_count = 1
-                    assert (
-                        len(rows) == expected_count
-                    ), f"Test failed for {sql_file}: Expectation differences"
-                print(f"Test passed for {sql_file}")
+                    assert len(rows) == expected_count, (
+                        f"Test failed for {sql_file}: Expected {expected_count} but found {len(rows)}"
+                    )
 
-                # Confirmation Rate
                 if "1934" in sql_file:
                     expected_count = 4
-                    assert (
-                        len(rows) == expected_count
-                    ), f"Test failed for {sql_file}: Expected {expected_count} but found {len(rows)}"
+                    assert len(rows) == expected_count, (
+                        f"Test failed for {sql_file}: Expected {expected_count} but found {len(rows)}"
+                    )
 
-                # Not boring movies
                 if "620" in sql_file:
                     expected_count = 2
-                    assert (
-                        len(rows) == expected_count
-                    ), f"Test failed for {sql_file}: Expectation differences"
+                    assert len(rows) == expected_count, (
+                        f"Test failed for {sql_file}: Expected {expected_count} but found {len(rows)}"
+                    )
 
-                # Average Selling Price
                 if "1251" in sql_file:
                     expected_count = 2
-                    assert (
-                        len(rows) == expected_count
-                    ), f"Test failed for {sql_file}: Expectation differences"
+                    assert len(rows) == expected_count, (
+                        f"Test failed for {sql_file}: Expected {expected_count} but found {len(rows)}"
+                    )
 
-                # Project Employees
                 if "1075" in sql_file:
                     expected_count = 2
-                    assert (
-                        len(rows) == expected_count
-                    ), f"Test failed for {sql_file}: Expectation differences"
+                    assert len(rows) == expected_count, (
+                        f"Test failed for {sql_file}: Expected {expected_count} but found {len(rows)}"
+                    )
 
-                # Percentage of Users attended a contest
                 if "1633" in sql_file:
                     expected_count = 5
-                    assert (
-                        len(rows) == expected_count
-                    ), f"Test failed for {sql_file}: Expectation differences"
+                    assert len(rows) == expected_count, (
+                        f"Test failed for {sql_file}: Expected {expected_count} but found {len(rows)}"
+                    )
 
-                # Queries Quality and Percentage
                 if "1211" in sql_file:
                     expected_count = 2
-                    assert (
-                        len(rows) == expected_count
-                    ), f"Test failed for {sql_file}: Expectation differences"
+                    assert len(rows) == expected_count, (
+                        f"Test failed for {sql_file}: Expected {expected_count} but found {len(rows)}"
+                    )
 
-                # Monthly Transactions I
                 if "1193" in sql_file:
                     expected_count = 3
-                    assert (
-                        len(rows) == expected_count
-                    ), f"Test failed for {sql_file}: Expected {expected_count} but found {len(rows)}."
-                    
+                    assert len(rows) == expected_count, (
+                        f"Test failed for {sql_file}: Expected {expected_count} but found {len(rows)}"
+                    )
+
                 # Immediate Food Delivery II
                 if "1174" in sql_file:
+                    # If it's expected to return a single value
                     row = result.fetchone()
+                    if row is None:
+                        raise ValueError(f"No data returned for query: {sql_query}")
                     value = row[0]
                     expected_value = 50.00
-                    assert (
-                        value == expected_value
-                    ), f"Test failed for {sql_file}: Expected {expected_value} but got {value}."
+                    assert abs(value - expected_value) < 0.01, (
+                        f"Test failed for {sql_file}: Expected {expected_value} but got {value}."
+                    )
 
+            print(f"Test passed for {sql_file}")
 
-        finally:
-            conn.close()
-
+        except Exception as e:
+            print(f"An error occurred while processing {sql_file}: {e}")
 
 def test_queries():
     # Get the path to the leetcode folder
@@ -192,7 +169,6 @@ def test_queries():
 
     # Run tests on all SQL files
     run_tests(sql_files)
-
 
 if __name__ == "__main__":
     test_queries()
